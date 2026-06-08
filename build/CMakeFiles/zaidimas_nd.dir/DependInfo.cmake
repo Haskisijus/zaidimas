643910a767376eda1a2d628754415e24ebfb2dfa
Deividas <deividassiniauskas@gmail.com>
2026-06-08 20:04:14 +0300

    pakeitimai

---
+++ build/CMakeFiles/zaidimas_nd.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/deividas/CLionProjects/zaidimas/src/asset_manager.cpp" "CMakeFiles/zaidimas_nd.dir/src/asset_manager.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/asset_manager.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/button.cpp" "CMakeFiles/zaidimas_nd.dir/src/button.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/button.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/graphics_utils.cpp" "CMakeFiles/zaidimas_nd.dir/src/graphics_utils.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/graphics_utils.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/label.cpp" "CMakeFiles/zaidimas_nd.dir/src/label.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/label.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/leaderboard_manager.cpp" "CMakeFiles/zaidimas_nd.dir/src/leaderboard_manager.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/leaderboard_manager.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/main.cpp" "CMakeFiles/zaidimas_nd.dir/src/main.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/main.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/panel.cpp" "CMakeFiles/zaidimas_nd.dir/src/panel.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/panel.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/score_storage.cpp" "CMakeFiles/zaidimas_nd.dir/src/score_storage.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/score_storage.cpp.o.d"
+  "/home/deividas/CLionProjects/zaidimas/src/utils.cpp" "CMakeFiles/zaidimas_nd.dir/src/utils.cpp.o" "gcc" "CMakeFiles/zaidimas_nd.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/zaidimas_nd.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zaidimas_nd.dir/src/asset_manager.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/asset_manager.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/button.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/button.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/graphics_utils.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/graphics_utils.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/label.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/label.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/leaderboard_manager.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/leaderboard_manager.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/main.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/main.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/panel.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/panel.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/score_storage.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/score_storage.cpp.o.d"
+  "CMakeFiles/zaidimas_nd.dir/src/utils.cpp.o"
+  "CMakeFiles/zaidimas_nd.dir/src/utils.cpp.o.d"
+  "zaidimas_nd"
+  "zaidimas_nd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zaidimas_nd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
